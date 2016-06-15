2952423d69bcbf3058d082958564f644dfe8b22d
Adrian Szymański <aszymanski@infusion.com>
2016-06-15 19:48:51 +0200

    Added BMI calculation formula

--- Java/WindowBuilderTest1/src/app/redape/com/WindowBuilderTest.java
+++ Java/WindowBuilderTest1/src/app/redape/com/WindowBuilderTest.java
@@ -14,6 +14,8 @@ import javax.swing.JTextField;
 import javax.swing.JLabel;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.text.DecimalFormat;
+import javax.swing.JFormattedTextField;
 
 public class WindowBuilderTest extends JFrame {
 
@@ -45,47 +47,59 @@ public class WindowBuilderTest extends JFrame {
 	 */
 	public WindowBuilderTest() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 636, 507);
+		setBounds(100, 100, 388, 241);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JButton btnCalculate = new JButton("Calculate");
-		btnCalculate.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				textField.setText("Results");;
+		btnCalculate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		btnCalculate.setBounds(254, 394, 97, 25);
+		btnCalculate.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent click) {
+				
+				double result = Double.valueOf(weight.getText())/(Double.valueOf(height.getText())*Double.valueOf(height.getText()))*10000;										
+				DecimalFormat df = new DecimalFormat("#.00");
+				String resultStr  = df.format(result);
+				textField.setText(resultStr);
+			}
+		});
+		btnCalculate.setBounds(149, 145, 97, 25);
 		contentPane.add(btnCalculate);
 		
 		weight = new JTextField();
-		weight.setBounds(118, 33, 116, 22);
+		weight.setBounds(149, 33, 116, 22);
 		contentPane.add(weight);
 		weight.setColumns(10);
 		
-		JLabel lblNewLabel = new JLabel("Weight");
-		lblNewLabel.setBounds(50, 36, 56, 16);
+		JLabel lblNewLabel = new JLabel("Weight [kg]");
+		lblNewLabel.setBounds(50, 36, 71, 16);
 		contentPane.add(lblNewLabel);
 		
 		height = new JTextField();
-		height.setBounds(118, 72, 116, 22);
+		height.setBounds(149, 66, 116, 22);
 		contentPane.add(height);
 		height.setColumns(10);
 		
-		lblNewLabel_1 = new JLabel("Height");
-		lblNewLabel_1.setBounds(50, 75, 56, 16);
+		lblNewLabel_1 = new JLabel("Height [cm]");
+		lblNewLabel_1.setBounds(50, 69, 71, 16);
 		contentPane.add(lblNewLabel_1);
 		
 		lblBmi = new JLabel("BMI");
-		lblBmi.setBounds(50, 115, 56, 16);
+		lblBmi.setBounds(50, 102, 71, 16);
 		contentPane.add(lblBmi);
 		
 		textField = new JTextField();
-		textField.setBounds(118, 112, 116, 22);
+		textField.setBounds(149, 99, 116, 22);
 		contentPane.add(textField);
 		textField.setColumns(10);
+		
+		JLabel lblBodyMassIndex = new JLabel("Body Mass Index Calculator");
+		lblBodyMassIndex.setBounds(118, 8, 168, 14);
+		contentPane.add(lblBodyMassIndex);
 	}
 }
